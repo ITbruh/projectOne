2ef6f48f51e9cbeb01555866fbbe96c2d9838caf
Fedos <rskankov@gmail.com>
2023-02-05 17:23:30 +0800

    first commit

---
+++ startjs/js/script.js
@@ -0,0 +1,41 @@
+"use strict";
+
+let number = 5;
+const leftBorderWidth = 7;
+
+let lessonTwo = 5;
+console.log(lessonTwo * 5)
+console.log(5/0) //infinite(бесконечность, потому что неравенство не правильное)
+console.log('string' * 222) //NaN то есть 'это не число' потому что мы умножаем строку на число
+
+
+let obj = {
+    name: 'Lexa',
+    age: 25,
+    isMarried: false
+}
+
+console.log(obj.name)
+
+let arr = [
+    'bruh',
+    4,
+    'chert',
+    true
+]
+
+console.log(arr[2])
+
+// const lessonThree = confirm('che tam?');
+// console.log(lessonThree);
+
+// const answers = [];
+
+// answers[0] = prompt('Ты кто епт?', '');
+// answers[1] = prompt('Ты где епт?', '');
+// answers[2] = prompt('Ты от куда епт?', '');
+// document.write(answers);
+
+let Name = 'Fed';
+
+alert(`Hi, ${Name}`)
